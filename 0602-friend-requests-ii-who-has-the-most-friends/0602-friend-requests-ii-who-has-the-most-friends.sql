8ada4f7c0df0d73b733224b7d48b118aa6729e11
Janani1495 <jananidhanashekaran@gmail.com>
2025-07-31 18:19:01 +0530

    Time: 275 ms (94.46%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -2,4 +2,4 @@
 with ans as (select requester_id id from RequestAccepted 
 union all
 select accepter_id id from RequestAccepted)
-select id,count(*)num from ans group by 1 order by 2 desc limit 1;
+select id,count(*)num from ans group by id order by num desc limit 1;
