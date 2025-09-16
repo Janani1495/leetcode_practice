452a606cda5c2b5c90b13a46ed21a04ad7c49d65
Janani1495 <jananidhanashekaran@gmail.com>
2025-09-16 13:57:41 +0530

    Time: 56 ms (23.06%), Space: 74.1 MB (43.19%) - LeetHub

---
+++ 3541-report-spam-message/3541-report-spam-message.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        HashSet<String>set=new HashSet<>();
+        for(int i=0;i<bannedWords.length;i++){
+            set.add(bannedWords[i]);
+        }int count=0;
+        for(int i=0;i<message.length;i++){
+            if(set.contains(message[i])){
+                count++;
+            }
+        }return count>=2;
+    }
+}
