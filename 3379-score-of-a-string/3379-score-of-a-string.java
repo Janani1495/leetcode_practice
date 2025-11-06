d4e344c46fc235f31264f1f340936e2080b64f60
Janani1495 <jananidhanashekaran@gmail.com>
2025-11-06 12:28:52 +0530

    Time: 1 ms (99.26%), Space: 43.5 MB (5.64%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int[]arr=new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            arr[i]=(int)(s.charAt(i));
+        }
+        int ans=0;
+        for(int i=1;i<s.length();i++){
+            ans+=Math.abs(arr[i]-arr[i-1]);
+        }return ans;
+    }
+}
